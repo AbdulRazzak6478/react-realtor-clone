cad3b88d63f12003af999cafcaa3a6d9832aa17d
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-01-25 01:32:44 +0530

    add functionality including onChange and onSubmit functions to the create listing page

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import ForgetPassword from "./pages/ForgetPassword";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-import {Toaster} from 'react-hot-toast';
+import "react-toastify/dist/ReactToastify.css";
+import { Toaster } from "react-hot-toast";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 function App() {
@@ -20,13 +20,15 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<PrivateRoute />}>
-              <Route path="/profile" element={<Profile />} />
+            <Route path="/profile" element={<Profile />} />
           </Route>
           <Route path="/offers" element={<Offers />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forget-password" element={<ForgetPassword />} />
-          <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/create-listing" element={<PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
         </Routes>
       </Router>
       <Toaster />
